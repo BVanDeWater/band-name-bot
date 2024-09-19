0ed53809f21efcda8fe7e9a0e6bbaa7f899f2d1e
Ben Van De Water <Ben.VanDeWater@gmail.com>
2024-09-19 15:22:14 -0400

    Fixed word retrieval and handling.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dataclasses import replace
-from english_words import english_words_set
+from english_words import get_english_words_set
 from numpy.random import poisson
 import random
 from time import sleep
@@ -12,8 +12,9 @@ import templates
 ########
 
 def main():
+    ws = list(get_english_words_set(["web2"]))
     while(True):
-        tweet = generate_tweet()
+        tweet = generate_tweet(ws)
 
         print(tweet)
         sleep(1)
@@ -23,25 +24,25 @@ def main():
 # GENERATE FUNCTIONS #
 ######################
 
-def generate_tweet():
+def generate_tweet(word_set):
     tweet = random.choice(templates.tweets)
-    tweet = tweet.replace("<BANDNAME>", generate_name())
-    tweet = tweet.replace("<ALBUMNAME>", generate_album())
+    tweet = tweet.replace("<BANDNAME>", generate_name(word_set))
+    tweet = tweet.replace("<ALBUMNAME>", generate_album(word_set))
     return tweet
 
-def generate_name():
+def generate_name(word_set):
     n_words = poisson(1)
     name = ""
     for word in range(n_words):
-        name += random.choice(list(english_words_set)).capitalize() + " "
-    return name + random.choice(list(english_words_set)).capitalize()
+        name += random.choice(word_set).capitalize() + " "
+    return name + random.choice(word_set).capitalize()
 
-def generate_album():
+def generate_album(word_set):
     n_words = poisson(3)
     name = ""
     for word in range(n_words):
-        name += random.choice(list(english_words_set)).capitalize() + " "
-    return name + random.choice(list(english_words_set)).capitalize()
+        name += random.choice(word_set).capitalize() + " "
+    return name + random.choice(word_set).capitalize()
 
 def generate_merch():
     pass
